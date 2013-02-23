4419c4f6e34a349b9e70413b27f695cce9de8212
Colm Heaney <colm.heaney@gmail.com>
2013-02-23 13:23:22 +0000

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,27 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-	describe "home page" do
-		it "should have the content 'IAR'" do
-			visit '/static_pages/home'
-			page.should have_selector('h1', :text => 'Home')
-		end
+	subject { page }
 
-		it "should have the right title" do
-			visit '/static_pages/home'
-			page.should have_selector('title', :text => "Irish Archaeological Research | Home")
-		end
+	describe "home page" do
+		before { visit root_path }
+
+		it { should have_selector('h1', text: 'Welcome to IAR') }
+		it { should have_selector('title', text: full_title('')) }
+		it { should_not have_selector('title', text: '| Home') }
 	end
 
 	describe "contact page" do
-		it "should have the content 'Contact" do
-			visit '/static_pages/contact'
-			page.should have_selector('h1', :text => 'Contact Us')
-		end
+		before { visit contact_path }
 
-		it "should have the right title" do
-			visit '/static_pages/contact'
-			page.should have_selector('title', :text => "Irish Archaeological Research | Contact")
-		end
+		it { should have_selector('h1', text: 'Contact Us') }
+		it { should have_selector('title', text: full_title('Contact Us')) }
 	end
 
 	describe "about page" do
-		it "should have the content 'About" do
-			visit '/static_pages/about'
-			page.should have_selector('h1', :text => 'About Us')
-		end
+		before { visit about_path }
 
-		it "should have the right title" do
-			visit '/static_pages/about'
-			page.should have_selector('title', :text => "Irish Archaeological Research | About")
-		end
+		it { should have_selector('h1', text: 'About Us') }
+		it { should have_selector('title', text: full_title('About Us')) }
 	end
 end
