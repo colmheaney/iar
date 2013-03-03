bd54731bf8d1a58a8c8d6bd2be3519844d9823cd
Colm Heaney <colm.heaney@gmail.com>
2013-03-03 00:08:17 +0000

    some desparate code changes to make heroku work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
     @post.user_id = current_user.id
 
     if @post.save
-        redirect_to post_path(@post), notice: 'Post was successfully created.'
+        redirect_to post_path(@post), notice: 'Post was successfully created'
     else
         render action: "new"
     end
