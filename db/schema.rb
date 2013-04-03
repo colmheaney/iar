2ec44af422754b5bc4b927551c9fe152e528fcac
Colm <colm.heaney@gmail.com>
2013-04-03 22:01:57 +0100

    changed h4's to h5's

--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(:version => 20130320231131) do
     t.text     "description", :limit => 255
     t.string   "location"
     t.date     "date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.integer  "news"
   end
 
