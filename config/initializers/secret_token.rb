8fb3af39a03559f05d1e02b0321d60727d0bce9e
Colm <colm.heaney@gmail.com>
2013-04-29 21:36:27 +0100

    fixing secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Iar::Application.config.secret_token = ''
+Iar::Application.config.secret_token = 'b3e2f6981eff7529448013802de87d28a171c2f5239e8f905f0cc15628cd699b6d1019d4191c13e6b1125ba07a1b7fddfb405d8a00596bbe47d20b5ed9fbd226'
