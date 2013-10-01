d1ea6c6b963e3bf790aa0682fbdc9855f89c95c7
Colm <colm.heaney@gmail.com>
2013-10-01 22:13:15 +0100

    some aerial photos

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,6 @@ class ContactMailer < ActionMailer::Base
 	def user_contact(message)
 		@message = message
 		subject = message.name + " (" + message.email + ")"
-		mail(:to => "colm.heaney@btinternet.com", :subject => subject, :body => message.content, :from => "colm.heaney@gmail.com")
+		mail(:to => "info@irisharchaeologicalresearch.com", :subject => subject, :body => message.content, :from => "colm.heaney@gmail.com")
 	end
 end
